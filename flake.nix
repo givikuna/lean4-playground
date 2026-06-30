6aea68d50e50a9d993a05b06884fbc779004dd86
Givi <gvtsvariani@gmail.com>
2026-06-29 22:03:12 -0400

    env

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Basic Lean 4 development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            lean
+          ];
+
+          shellHook = "";
+        };
+      }
+    );
+}
